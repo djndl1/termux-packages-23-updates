db173499eb86e3bc6177b5368ec08fe1acc0c9c2
djndl1 <djndl1@gmail.com>
2025-07-21 20:47:16 +0800

    Revert "fix(elf-cleaner): delete extra arguments"
    This reverts commit 5408ef7a5eb464fa5b2dac8e8128f018639e20e0.
    We now have modern termux-elf-cleaner

--- scripts/build/termux_step_elf_cleaner.sh
+++ scripts/build/termux_step_elf_cleaner.sh
@@ -5,5 +5,5 @@ termux_step_elf_cleaner() {
 termux_step_elf_cleaner__from_paths() {
 	# Remove entries unsupported by Android's linker:
 	find "$@" -type f -print0 | xargs -r -0 \
-		"$TERMUX_ELF_CLEANER"
+		"$TERMUX_ELF_CLEANER" --api-level "$TERMUX_PKG_API_LEVEL"
 }
