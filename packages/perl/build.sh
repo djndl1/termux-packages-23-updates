ff44c8c50c81c18c30f4ca5297152675b4661e99
djndl1 <djndl1@gmail.com>
2025-07-27 02:30:40 +0800

    newpkg(perl): fix pipe2 error

--- packages/perl/build.sh
+++ packages/perl/build.sh
@@ -10,23 +10,27 @@ TERMUX_PKG_MAINTAINER="@termux"
 # - psutils
 # - subversion
 TERMUX_PKG_VERSION=5.38.4
-TERMUX_PKG_SRCURL="https://www.cpan.org/src/5.0/perl-5.38.4.tar.gz"
+TERMUX_PKG_SRCURL="https://github.com/Perl/perl5/archive/refs/tags/v5.38.4.tar.gz"
 TERMUX_PKG_SHA256=e63ee4f816ccdba64cecf9036a80da645b79519bf54e4c89832497a16088556e
 TERMUX_PKG_DEPENDS=libandroid-utimes
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_configure() {
 		cd $TERMUX_PKG_BUILDDIR
-		CFLAGS=" -D__USE_BSD=1" LDFLAGS=" -Wl,-rpath=$TERMUX_PREFIX/lib -L$TERMUX_PREFIX/lib -landroid-utimes -lm" $TERMUX_PKG_SRCDIR/configure \
+		CFLAGS+=" -D__USE_GNU"
+		LDFLAGS=" -Wl,-rpath=$TERMUX_PREFIX/lib -L$TERMUX_PREFIX/lib -landroid-utimes -lm" $TERMUX_PKG_SRCDIR/Configure \
+			-des \
+			-Alibpth="/system/lib /vendor/lib" \
 			-Dsysroot=$TERMUX_PREFIX \
 			-Dprefix=$TERMUX_PREFIX \
-			-Dsh=$TERMUX_PREFIX/bin/sh \
 			-Dld="$CC -Wl,-rpath=$TERMUX_PREFIX/lib -Wl,--enable-new-dtags" \
-			-Duseshrplib \
-			-Duseithreads \
-			-Dusemultiplicity \
-			-Doptimize="-O2" \
-			--with-libs="-lm -L$TERMUX_PREFIX/lib -landroid-utimes"
+			-Doptimize="-O2"
+}
+
+termux_step_make() {
+	echo '#define __USE_GNU' >> $TERMUX_PKG_SRCDIR/config.h
+	export CFLAGS
+	make CFLAGS="${CFLAGS}" -j $TERMUX_PKG_MAKE_PROCESSES
 }
 
 termux_step_post_make_install() {
